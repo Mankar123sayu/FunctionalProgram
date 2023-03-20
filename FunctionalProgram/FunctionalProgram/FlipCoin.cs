3bc37533fbbd62796290b3f45144de9eff5c5522
Mankar123sayu <sayumankar77@gmail.com>
2023-03-20 22:05:05 +0530

    AddFile

---
+++ FunctionalProgram/FunctionalProgram/FlipCoin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FunctionalPrograms
+{
+        public class FlipCoin
+        {
+            public void Flip(int noOfTimes)
+            {
+                int head = 0;
+                int tail = 0;
+                int headcount = 0;
+                int tailcount = 0;
+
+                if (noOfTimes > 0)
+                {
+                    for (int i = 0; i < noOfTimes; i++)
+                    {
+                        Random random = new Random();
+                        int coin = random.Next(2);
+
+                        if (coin == 0)
+                        {
+                            Console.WriteLine("Its Head!");
+                            head = 1;
+                            headcount += 1;
+                        }
+                        else if (coin == 1)
+                        {
+                            Console.WriteLine("Its Tail!");
+                            tail = 1;
+                            tailcount += 1;
+                        }
+                    }
+                }
+                double headPercentage = (headcount * 100) / noOfTimes;
+                double tailPercentage = (tailcount * 100) / noOfTimes;
+                Console.WriteLine($"Head Percentage ----> {headPercentage} \n Tail Percentage ----> {tailPercentage}");
+            }
+        }
+}
---
+++ FunctionalProgram/FunctionalProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using FunctionalPrograms;
+using System;
+
+namespace FunctionalPrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Functional Programs");
+
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose an option to execute \n 1. Flip Coin \n 2. LeapYear \n 3. PowerOf2 \n 4. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FlipCoin flip = new FlipCoin();
+                        Console.WriteLine("Enter num of flips: ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        flip.Flip(num);
+                        break;
+                }
+            }
+        }
+    }
+}
